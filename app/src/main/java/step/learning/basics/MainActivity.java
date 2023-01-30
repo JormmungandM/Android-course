317d3752d67783775e2ca69d6cad52dad11cb6bd
JormmungandM <misha.bond1432@gmail.com>
2023-01-30 12:08:20 +0200

    Class&Homework

--- app/src/main/java/step/learning/basics/MainActivity.java
+++ app/src/main/java/step/learning/basics/MainActivity.java
@@ -1,8 +1,10 @@
 package step.learning.basics;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button addButton = findViewById(R.id.addButton);
+        addButton.setOnClickListener(this::addButtonClick);
+    }
+
+    private void addButtonClick(View v){
+        TextView tvHello = findViewById(R.id.tvHello);
+        String txt = tvHello.getText().toString();
+        txt+="!";
+        tvHello.setText(txt);
     }
 }
